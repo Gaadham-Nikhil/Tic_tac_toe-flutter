95f80b9e99a72f414868766a1105493cdc31678e
Gaadham-Nikhil <nikhil2005outlook@gmail.com>
2024-10-17 23:05:58 +0530

    added button functionalites

--- lib/main.dart
+++ lib/main.dart
@@ -24,8 +24,55 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class TicTacToe extends StatelessWidget {
+class TicTacToe extends StatefulWidget {
   const TicTacToe({super.key});
+
+  @override
+  State<TicTacToe> createState() => _TicTacToeState();
+}
+
+class _TicTacToeState extends State<TicTacToe> {
+  String  gameWinner = '' ;
+  bool isCrossTurn = false;
+  List gameState = List.filled(9, 'empty');
+
+  void checkWinner() {
+    if (gameState[0] == gameState[1] &&
+        gameState[0] == gameState[2] &&
+        gameState[0] != 'empty') {
+      gameWinner = '${gameState[0]} won the game! 🥳';
+    } else if (gameState[3] != 'empty' &&
+        gameState[3] == gameState[4] &&
+        gameState[4] == gameState[5]) {
+      gameWinner = '${gameState[3]} won the game! 🥳';
+    } else if (gameState[6] != 'empty' &&
+        gameState[6] == gameState[7] &&
+        gameState[7] == gameState[8]) {
+      gameWinner = '${gameState[6]} won the game! 🥳';
+    } else if (gameState[0] != 'empty' &&
+        gameState[0] == gameState[3] &&
+        gameState[3] == gameState[6]) {
+      '${gameState[0]} won the game! 🥳';
+    } else if (gameState[1] != 'empty' &&
+        gameState[1] == gameState[4] &&
+        gameState[4] == gameState[7]) {
+      gameWinner = '${gameState[1]} won the game! 🥳';
+    } else if (gameState[2] != 'empty' &&
+        gameState[2] == gameState[5] &&
+        gameState[5] == gameState[8]) {
+      gameWinner = '${gameState[2]} won the game! 🥳';
+    } else if (gameState[0] != 'empty' &&
+        gameState[0] == gameState[4] &&
+        gameState[4] == gameState[8]) {
+      gameWinner = '${gameState[0]} won the game! 🥳';
+    } else if (gameState[2] != 'empty' &&
+        gameState[2] == gameState[4] &&
+        gameState[4] == gameState[6]) {
+      gameWinner = '${gameState[2]} won the game! 🥳';
+    } else if (!gameState.contains('empty')) {
+      gameWinner = 'Draw game... ⌛';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,13 +91,11 @@ class TicTacToe extends StatelessWidget {
       ),
       body: Column(
         children: [
-          const Center(
-            child: Card(
-              child: ListTile(
-                title: Center(
-                  child: Text(
-                    "Player-1: X",
-                  ),
+          Card(
+            child: ListTile(
+              title: Center(
+                child: Text(
+                  isCrossTurn? "Player-Chance: X" : "Player-Chance: O" 
                 ),
               ),
             ),
@@ -60,7 +105,19 @@ class TicTacToe extends StatelessWidget {
             itemCount: 9,
             gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 3),
-            itemBuilder: (context, index) => Container(decoration: BoxDecoration(border: Border.all(color: Colors.white)),child: const Center(child: Text("x"))),
+            itemBuilder: (context, index) => GestureDetector(
+              onTap: () {
+                if(gameState[index] == 'empty'){
+                  gameState[index] = isCrossTurn ? 'cross':'circle';
+                  isCrossTurn = !isCrossTurn;
+                  setState(() {});
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Cell is already filled')));
+                }
+                checkWinner();
+              },
+              child: TicTacToeCell(value : gameState[index])
+            ),
           )),
           Padding(
             padding: const EdgeInsets.all(16.0),
@@ -81,6 +138,32 @@ class TicTacToe extends StatelessWidget {
           )
         ],
       ),
+    );
+  }
+}
+
+class TicTacToeCell extends StatelessWidget {
+  final String value;
+  const TicTacToeCell({
+    super.key,
+    required this.value,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    Icon cellIcon;
+    switch (value) {
+      case 'cross':
+        cellIcon = const Icon(Icons.close_outlined);
+        break;
+      case 'circle':
+        cellIcon = const Icon(Icons.circle_outlined);
+      default:
+      cellIcon = const Icon(Icons.edit);
+    }
+    return Container(
+      decoration: BoxDecoration(border: Border.all(color: Colors.white)),
+      child: Center(child: cellIcon)
     );
   }
 }
